128f65390c7867397b64b9171e54df4b71ca52f5
yuxichen01 <38366360+yuxichen01@users.noreply.github.com>
2018-05-02 21:07:41 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+  <script>
+   $("#text").append("this is so freaking cool.");
+  </script>
